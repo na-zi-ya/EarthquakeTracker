ad9ac104b50f5638a5c8488912a7487cf84af432
naz-i-ya <naziyanaz1208@gmail.com>
2023-07-04 15:15:14 +0530

    Fixed: "Geojson with file system".

---
+++ filesystem/convertToGeoJSON.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+const earthquakeData = require('./earthquakeData.json');
+
+const convertToGeoJSON = () => {
+  const geojsonData = {
+    type: 'FeatureCollection',
+    features: earthquakeData.Infogempa.gempa.map((quake) => ({
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: quake.Coordinates.split(',').map(Number),
+      },
+      properties: {
+        name: quake.Wilayah,
+        dateTime: quake.DateTime,
+        magnitude: quake.Magnitude,
+      },
+    })),
+  };
+
+  fs.writeFile('earthquakeData.geojson', JSON.stringify(geojsonData, null, 2), (err) => {
+    if (err) throw err;
+    console.log('GeoJSON file created successfully!');
+  });
+};
+
+convertToGeoJSON();
--- map/src/components/Map.js
+++ map/src/components/Map.js
@@ -1,53 +1,57 @@
-import React from "react";
-import {
-  MapContainer,
-  TileLayer,
-  GeoJSON,
-  CircleMarker,
-  Popup,
-  FeatureGroup,
-  LayersControl,
-} from "react-leaflet";
-import tectonicPlates from "./PB2002_boundaries.json";
+import React, { useEffect, useState } from "react";
+import { MapContainer, TileLayer, GeoJSON, CircleMarker, Popup, FeatureGroup, LayersControl } from "react-leaflet";
+import tectonicPlates from './PB2002_boundaries.json';
+import data from '../geojson.json';
 import "leaflet/dist/leaflet.css";
-import { mapHeight, tectonicPlatesStyle, tileLayers } from "./tileLayer";
+import { tectonicPlatesStyle, tileLayers } from './tileLayer';
+
 
 function Map() {
+  const [geoJson, setGeoJson] = useState([]);
+
+  console.log(geoJson);
+
+  useEffect(() => {
+    setGeoJson(data);
+  }, [data]);
+
   const circleMarkerColor = (magnitude) => {
-    if (magnitude <= 1) return "#00b800";
-    if (magnitude > 1 && magnitude <= 2) return "#b6fe00";
-    if (magnitude > 2 && magnitude <= 3) return "#1cb981";
-    if (magnitude > 3 && magnitude <= 5) return "#ff9000";
-    if (magnitude > 5 && magnitude <= 7) return "#fedf17";
-    return "#A30000";
+    if (magnitude <= 1) return '#00b800';
+    if (magnitude > 1 && magnitude <= 2) return '#b6fe00';
+    if (magnitude > 2 && magnitude <= 3) return '#f6ff00';
+    if (magnitude > 3 && magnitude <= 5) return '#ffcf00';
+    if (magnitude > 5 && magnitude <= 7) return '#ff9000';
+    return '#ff0000';
   };
 
   return (
     <MapContainer center={[28.629093900641998, 77.09749706089497]} zoom={3}>
-      <LayersControl position="topright">
+       <LayersControl position="topright">
         {tileLayers.map(({ id, name, attribution, url, checked }) => (
           <LayersControl.BaseLayer key={id} name={name} checked={checked}>
             <TileLayer attribution={attribution} url={url} />
           </LayersControl.BaseLayer>
         ))}
         <LayersControl.Overlay name="Tectonic Plates">
-          <GeoJSON data={tectonicPlates} style={tectonicPlatesStyle} />
+          <GeoJSON
+            data={tectonicPlates}
+            style={tectonicPlatesStyle}
+          />
         </LayersControl.Overlay>
       </LayersControl>
       {/* <GeoJSON data={tectonicPlates} /> */}
       {/* <GeoJSON data={geoJson} style={geoJSONStyle} /> */}
-
-      <FeatureGroup color="purple">
-        <Popup>
-          <p>
-            DateTime: Tuesday, 4 July 2023 (IST)Time in Hyderabad, Telangana
-          </p>
-          <p>Name: Naziya</p>
-          <p>Magnitude: 3.0</p>
-          <p>Latitude: 17.385044</p>
-          <p>Longtitude: 78.486671</p>
-        </Popup>
-        {/* <Circle
+      {geoJson.features?.map((feature, index) => {
+          return (
+            <FeatureGroup color="purple" key={index}>
+              <Popup>
+                <p>DateTime: {feature.properties.dateTime}</p>
+                <p>Wilayah: {feature.properties.name}</p>
+                <p>Magnitude: {feature.properties.magnitude}</p>
+                <p>Latitude: {feature.geometry.coordinates[0]}</p>
+                <p>Longtitude: {feature.geometry.coordinates[1]}</p>
+              </Popup>
+              {/* <Circle
                 center={[
                   feature.geometry.coordinates[1],
                   feature.geometry.coordinates[0]
@@ -59,16 +63,22 @@ function Map() {
                 opacity={1}
                 fillOpacity={0.8}
               /> */}
-        <CircleMarker
-          // center={feature.geometry.coordinates[0,1]}
-          center={[17.385044, 78.486671]}
-          radius={12}
-          fillColor={circleMarkerColor(7)}
-          weight={0.5}
-          opacity={1}
-          fillOpacity={0.8}
-        ></CircleMarker>
-      </FeatureGroup>
+              <CircleMarker
+    // center={feature.geometry.coordinates[0,1]}
+    center={[
+      feature.geometry.coordinates[1], // latitude
+      feature.geometry.coordinates[0], // longitude
+    ]}
+    radius={12}
+    fillColor={circleMarkerColor(feature.properties.magnitude)}
+    weight={0.5}
+    opacity={1}
+    fillOpacity={0.8}>
+    </CircleMarker>
+             
+            </FeatureGroup>
+          );
+        })}
     </MapContainer>
   );
 }
